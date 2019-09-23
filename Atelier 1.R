3f4c063a76557621b7172b02460078f07379bb17
salomelacroix <55700726+salomelacroix@users.noreply.github.com>
2019-09-23 17:11:17 +0200

    Add files via upload

---
+++ Atelier 1.R
@@ -0,0 +1,24 @@
+2 + 4
+8 - 12
+14 * 25
+-3/10
+3.03-sqrt(2)
+x <- 2  
+x
+y<-seq(from=0,to=10^6,by=1)
+y[10000]
+tailles <- c(167, 192, 173, 174, 172, 167, 171, 185, 163, 170)
+tailles
+mean(tailles)#moyenne
+var(tailles) #variance
+sd(tailles) #ecar-type
+summary(tailles) #resume
+tailles^2
+sin(tailles)
+poids <- c(86, 74, 83, 50, 78, 66, 66, 51, 50, 55)
+length(tailles)
+length(poids)
+tailles.m <- tailles/100
+tailles.m
+imc <- poids/tailles.m^2
+imc
